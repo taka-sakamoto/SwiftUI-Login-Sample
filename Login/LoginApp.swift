ac8cfb0b630c9ecbfe3dcd4683c777dc4b3ab956
taka-sakamoto <goodspeed855@gmail.com>
2026-03-16 10:13:32 +0900

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Takayuki Sakamoto on 2026/03/16.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
